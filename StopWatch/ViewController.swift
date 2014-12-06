b6d1d7238131c1013c9366b877e27662b79cfeb6
EvoStar <yanlixiang90@gmail.com>
2014-12-06 10:02:42 -0800

    一个简单的stop watch
    实现button控制计时的功能

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var timeNumber: UILabel!
+    var count=0
+    var time = NSTimer()
+    
+    @IBAction func timeStarter(sender: AnyObject) {
+        
+    time=NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
+        
+}
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        
+        time.invalidate()
+        count=0
+        timeNumber.text = "0"
+        
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       
+    }
+    
+   func result(){
+        count++;
+        timeNumber.text = String(count)
+        
     }
 
     override func didReceiveMemoryWarning() {
